45a6aa346ef816ee9b82f32b27b56618d94a84ff
LeeTaeSoon <nrst136@naver.com>
2016-09-30 02:20:43 +0900

    divide some code related with html example
    make a new file 'page.js' and move some lines related with html from
    'index.js'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import Caculater from './calculator';	// import example calculator
-
-const c = new Caculater();
-
-const newDiv = document.createElement('h1');
-const br = document.createElement('br');
-
-const add = document.createTextNode(`4 + 2 = ${c.add(4, 2)}`);
-const sub = document.createTextNode(`4 - 2 = ${c.sub(4, 2)}`);
-
-newDiv.appendChild(add);
-newDiv.appendChild(br);
-newDiv.appendChild(sub);
-
-document.body.appendChild(newDiv);
+import show from './page'; // show example page
 
 export default Caculater;
