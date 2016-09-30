b78685b552e0b13f80e9df3ee35a5f1f253a1d8e
Jeff kang <sandfox2010@gmail.com>
2016-09-30 14:09:54 +0900

    Revert "divide some code related with html example"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import Caculater from './calculator';	// import example calculator
-import show from './page'; // show example page
+
+const c = new Caculater();
+
+const newDiv = document.createElement('h1');
+const br = document.createElement('br');
+
+const add = document.createTextNode(`4 + 2 = ${c.add(4, 2)}`);
+const sub = document.createTextNode(`4 - 2 = ${c.sub(4, 2)}`);
+
+newDiv.appendChild(add);
+newDiv.appendChild(br);
+newDiv.appendChild(sub);
+
+document.body.appendChild(newDiv);
 
 export default Caculater;
